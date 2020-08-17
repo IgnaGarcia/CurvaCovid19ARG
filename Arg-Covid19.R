cf8fad4fe675d67c5adcee6e3b58b87bf0c2888a
Igna <gnachoxp@gmail.com>
2020-08-17 00:00:08 -0300

    Vista por semana

--- Arg-Covid19.R
+++ Arg-Covid19.R
@@ -53,10 +53,13 @@ library(tidyr)
 ARG_COVID_19 <- ARG_COVID_19_H %>% gather(fecha, Acum_fi, 2:ncol(ARG_COVID_19_H))
 
 #Formatear fecha
-ARG_COVID_19$fecha <- as.Date(as.character(ARG_COVID_19$fecha), format = "X%m.%d.%y")
+ARG_COVID_19$fecha <- as.Date(ARG_COVID_19$fecha, format = "X%m.%d.%y")
+ARG_COVID_19$mes <- as.numeric(format(ARG_COVID_19$fecha,'%m'))
+ARG_COVID_19$semana <- strftime(ARG_COVID_19$fecha, format = "%V")
+ARG_COVID_19$dia <- as.numeric(format(ARG_COVID_19$fecha,'%d'))
 
 #Reordenar columnas
-ARG_COVID_19 <- sqldf("SELECT fecha, fi, Acum_fi FROM ARG_COVID_19")
+ARG_COVID_19 <- sqldf("SELECT mes, semana, fecha, fi, Acum_fi FROM ARG_COVID_19 WHERE mes > 2")
 
 #Agregar col para nuevos casos
 ARG_COVID_19$fr <- 0 
@@ -83,52 +86,43 @@ for (caso in ARG_COVID_19$Acum_fi) {
   i <- i+1
 }
 
+#####nuevas tablas con datos agrupados por mes y por semana
+
+## agrupamiento por mes
+XMES_COVID_19 <- sqldf( "select mes,
+   SUM(fi) fi,
+   MAX(Acum_fi) Acum_fi,
+   SUM(fr) fr ,
+   SUM(frPorcent) frPorcent,
+   MAX(Acum_fr) Acum_fr,
+   MAX(Acum_frPorcent) Acum_frPorcent
+                  from ARG_COVID_19
+                  GROUP BY mes
+                         ")
+
+XSEMANA_COVID_19 <- sqldf( "select semana, 
+   SUM(fi) fi,
+   MAX(Acum_fi) Acum_fi,
+   SUM(fr) fr ,
+   SUM(frPorcent) frPorcent,
+   MAX(Acum_fr) Acum_fr,
+   MAX(Acum_frPorcent) Acum_frPorcent
+                  from ARG_COVID_19
+                  GROUP BY semana
+                         ")
 
 #######
 #   grabar los datos en un archivo .csv
-write.csv2(ARG_COVID_19, "datos_ARG.csv",  row.names = FALSE, fileEncoding = "UTF-8")
-
-
-#substr(fecha, 6, 2)
-#TO_CHAR(fecha, 'MM')
-#EXTRACT(MONTH FROM fecha)
-
-# para graficar los mas importantes
-# XMES_COVID_19 <- sqldf( "select 
-#   fecha,
-#   EXTRACT(MONTH FROM fecha) mes,
-#   fi,
-#   Acum_fi,
-#   fr,
-#   frPorcent,
-#   Acum_fr,
-#   Acum_frPorcent
-#                  from ARG_COVID_19
-#                         ")
-# 
-# XMES_COVID_19 <- sqldf( "select 
-#   fecha,
-#   substr(fecha, 6, 2) mes,
-#   SUM(fi) as fi,
-#   MAX(Acum_fi) as Acum_fi,
-#   SUM(fr) as fr,
-#   SUM(frPorcent) as frPorcent,
-#   MAX(Acum_fr) as Acum_fr,
-#   MAX(Acum_frPorcent) as Acum_frPorcent
-#                  from ARG_COVID_19
-#                  GROUP BY mes
-#                         ")
-
-# XSEMANA_COVID_19 <- sqldf( "select EXTRACT(WEEK from a.fecha) semana, SUM(fi), MAX(Acum_fi), SUM(fr), SUM(frPorcent), MAX(Acum_fr), MAX(Acum_frPorcent)  
-#                  from ARG_COVID_19
-#                  GROUP BY semana")
+write.csv2(ARG_COVID_19, "datos_XDia_ARG.csv",  row.names = FALSE, fileEncoding = "UTF-8")
+write.csv2(XMES_COVID_19, "datos_XMes_ARG.csv",  row.names = FALSE, fileEncoding = "UTF-8")
+write.csv2(XSEMANA_COVID_19, "datos_XSemana_ARG.csv",  row.names = FALSE, fileEncoding = "UTF-8")
 
 ############## genero  figura dinamica
+
 #Casos Diarios
 g1 <- ggplot(ARG_COVID_19 ,aes(x = fecha , y = fi) ) + 
-  geom_segment(size = 0.08, aes(xend = fecha, yend=0)) +
-  geom_line(color="blue") +
-  ggtitle("COVID_19 - Casos Diarios en Argentina") +
+  geom_line(color="orange") +
+  ggtitle("COVID-19 - Casos Diarios en Argentina") +
   theme(plot.title = element_text(lineheight = 1,face ='bold'))   +
   ylab("Casos diarios") +
   xlab("") +
@@ -136,8 +130,8 @@ g1 <- ggplot(ARG_COVID_19 ,aes(x = fecha , y = fi) ) +
   theme_minimal() +
   theme(axis.text.y = element_text(angle = 45, hjust = 1)) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
-  geom_point() +
-  geom_text(aes(label = round(fi,1)), position = position_stack(vjust = 1), check_overlap = TRUE)
+  geom_point(size= .1) #+
+  #geom_text(aes(label = round(fi,0)), position = position_stack(vjust = 1), check_overlap = TRUE)
 
 g1 <- ggplotly(g1, tooltip = c("casos")) %>%
   layout(legend = list(
@@ -148,19 +142,41 @@ g1
 
 
 
-#Casos diarios en %
-g3 <- ggplot(ARG_COVID_19 ,aes(x = fecha , y = frPorcent) ) +
-  geom_segment(size = 0.08, aes(xend = fecha, yend=0)) +
-  geom_line(color="green") +
-  ggtitle("COVID_19 - % Casos Diarios en Argentina") +
+#Casos Mensuales 
+g2 <- ggplot(XMES_COVID_19 ,aes(x = mes , y = fi) ) +
+  geom_line(color="red") +
+  geom_segment(size = 0.08, aes(xend = mes, yend=0)) +
+  ggtitle("COVID_19 - Casos Mensuales en Argentina") +
   theme(plot.title = element_text(lineheight = 1,face ='bold'))   +
-  ylab("Casos diarios") +
+  ylab("Casos Mensuales") +
   xlab("") +
   labs(caption = "\nFuente: The Johns Hopkins University Center for Systems Science and Engineering (JHU CSSE)") +
   theme_minimal() +
   theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
   theme(axis.text.y = element_text(angle = 45, hjust = 1)) +
+  geom_point(size= .1) +
   geom_text(aes(label = round(fi,1)), position = position_stack(vjust = 1))
+
+g2 <- ggplotly(g2, tooltip = c("casos")) %>%
+  layout(legend = list(
+    orientation = "h",
+    x = 0.7,
+    y = 0))
+g2
+
+#Casos Semanales 
+g3 <- ggplot(XSEMANA_COVID_19 ,aes(x = semana, y = fi) ) +
+  geom_line(color="blue") +
+  geom_segment(size = 0.08, aes(xend = semana, yend=0)) +
+  ggtitle("COVID_19 - Casos Semanales en Argentina") +
+  theme(plot.title = element_text(lineheight = 1,face ='bold'))   +
+  ylab("Casos Semanales") +
+  xlab("") +
+  labs(caption = "\nFuente: The Johns Hopkins University Center for Systems Science and Engineering (JHU CSSE)") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  theme(axis.text.y = element_text(angle = 45, hjust = 1)) +
+  geom_point(size= .1)
 
 g3 <- ggplotly(g3, tooltip = c("casos")) %>%
   layout(legend = list(
@@ -168,5 +184,3 @@ g3 <- ggplotly(g3, tooltip = c("casos")) %>%
     x = 0.7,
     y = 0))
 g3
-
-
